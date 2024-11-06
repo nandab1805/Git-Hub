af96a470643fdd3437a3e21ff85c7d87a00767fc
nandab1805 <balabhadranandakiranroyal@gmail.com>
2024-11-06 18:35:27 +0530

    del log script

---
+++ Shell-Scripting/14.diskusage.sh
@@ -0,0 +1,17 @@
+#!/bin/bash/
+
+data_usage=$(df -hT | grep -vE 'tmp|file')
+disk_threshold=1
+message=""
+
+while ifs=read line
+do
+    usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
+    parition=$(echo $line | awk '{print $6F}')
+    if [ $usage -gt $disk_threshold ]
+    then
+        message+="High Disk usage on $parition: $usage"
+    fi
+done <<<$disk_usage
+
+echo "Message: $message"
