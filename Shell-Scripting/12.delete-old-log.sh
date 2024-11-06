da906f4e8c70830a9d12d8687e6bf155dad2d2aa
nandab1805 <balabhadranandakiranroyal@gmail.com>
2024-11-06 14:33:16 +0530

    del log script

--- Shell-Scripting/12.delete-old-log.sh
+++ Shell-Scripting/12.delete-old-log.sh
@@ -8,8 +8,8 @@ fi
 
 files_to_delete=$(find $source_dir -type f -mtime +14 -name "*.log")
 
-while ifs= read -r line
+while ifs= read -r line #Internal filed seprater- ifs
 do
     echo "Deleting file: $line"
     rm -rf $line
-done <<< $files_to_delete
+done <<< $files_to_delete #Less than always denotes input
