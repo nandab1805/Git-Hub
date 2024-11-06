451ee4df0ee33ea3ee1938bf3152138e6f63f932
nandab1805 <balabhadranandakiranroyal@gmail.com>
2024-11-06 19:11:52 +0530

    del log script

--- Shell-Scripting/14.diskusage.sh
+++ Shell-Scripting/14.diskusage.sh
@@ -4,7 +4,7 @@ data_usage=$(df -hT | grep -vE 'tmp|file')
 disk_threshold=1
 message=""
 
-while IFS= read -r line
+while IFS= read  line
 do
   usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
   partition=$(echo "$line" | awk '{print $1}')
