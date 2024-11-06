54002c580d647fae4a69439cdbf2bea8fea0b730
nandab1805 <balabhadranandakiranroyal@gmail.com>
2024-11-06 18:50:00 +0530

    del log script

--- Shell-Scripting/14.diskusage.sh
+++ Shell-Scripting/14.diskusage.sh
@@ -8,7 +8,7 @@ while ifs= read -r line
 do
     usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
     parition=$(echo $line | awk '{print $1F}')
-    if [ $usage -ge $disk_threshold ]
+    if ["$usage" -ge "$disk_threshold" ]
     then
         message+="High Disk usage on $parition: $usage"
     fi
