8614381f8592a3aeadb63868e878632af650cb75
nandab1805 <balabhadranandakiranroyal@gmail.com>
2024-11-06 19:11:31 +0530

    del log script

--- Shell-Scripting/14.diskusage.sh
+++ Shell-Scripting/14.diskusage.sh
@@ -6,7 +6,7 @@ message=""
 
 while IFS= read -r line
 do
-  usage=$(echo "$line" | awk '{print $6F}' | cut -d % -f1)
+  usage=$(echo $line | awk '{print $6F}' | cut -d % -f1)
   partition=$(echo "$line" | awk '{print $1}')
   echo "$usage"
   echo "$partition"
