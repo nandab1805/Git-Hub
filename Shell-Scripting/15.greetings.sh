5d4c71a7d5e56356c17908e0ba3f786493335a6d
nandab1805 <balabhadranandakiranroyal@gmail.com>
2024-11-07 20:11:29 +0530

    adding script

--- Shell-Scripting/15.greetings.sh
+++ Shell-Scripting/15.greetings.sh
@@ -4,15 +4,14 @@ name=""
 wishes=""
 
 usage(){
-    echo "usage:: $(basename $0) -n <name> -w<wishes>"
-    echo "Options"
+    echo "usage:: $(basename $0) -n <name> -w <wishes>"
+    echo "Options::"
     echo " -n, specify the name (mandatory)"
     echo " -w, specify the wishes. ex, Good Morning"
     echo " -h, Display Help and exit"
 
 }
-while getopts ":n:w:h" opt;
-do
+while getopts ":n:w:h" opt;do
     case $opt in
         n) name="$optarg";;
         w) wishes="$optarg";;
