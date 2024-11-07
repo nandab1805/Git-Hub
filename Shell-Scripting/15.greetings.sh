a8d68cdc1df49388f0e83a83be2143ff1985e098
nandab1805 <balabhadranandakiranroyal@gmail.com>
2024-11-07 20:12:11 +0530

    adding script

--- Shell-Scripting/15.greetings.sh
+++ Shell-Scripting/15.greetings.sh
@@ -3,26 +3,29 @@
 name=""
 wishes=""
 
-usage(){
-    echo "usage:: $(basename $0) -n <name> -w <wishes>"
-    echo "Options::"
+usage() {
+    echo "Usage: $(basename $0) -n <name> -w <wishes>"
+    echo "Options:"
     echo " -n, specify the name (mandatory)"
-    echo " -w, specify the wishes. ex, Good Morning"
-    echo " -h, Display Help and exit"
-
+    echo " -w, specify the wishes, e.g., 'Good Morning'"
+    echo " -h, display help and exit"
 }
-while getopts ":n:w:h" opt;do
+
+while getopts ":n:w:h" opt; do
     case $opt in
-        n) name="$optarg";;
-        w) wishes="$optarg";;
-        \?) echo "invaild options: -"$optarg"" >&2; usage; exit;;
-        :) usage; exit;; 
+        n) name="$OPTARG";;
+        w) wishes="$OPTARG";;
         h) usage; exit;;
+        \?) echo "Invalid option: -$OPTARG" >&2; usage; exit 1;;
+        :) echo "Option -$OPTARG requires an argument." >&2; usage; exit 1;;
     esac
 done
 
+# Debugging line to check the values of $name and $wishes
+echo "Debug: name='$name', wishes='$wishes'"
+
 if [ -z "$name" ] || [ -z "$wishes" ]; then
-    echo "Error: Both -n and -w are mandatory option."
+    echo "Error: Both -n and -w are mandatory options."
     usage
     exit 1
 fi
