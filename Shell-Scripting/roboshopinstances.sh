c213a8777ad6cec92f925aaa8e92b9a09d6c2786
nandab1805 <balabhadranandakiranroyal@gmail.com>
2024-11-08 18:07:38 +0530

    Adding scripts

--- Shell-Scripting/roboshopinstances.sh
+++ Shell-Scripting/roboshopinstances.sh
@@ -7,7 +7,7 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 for i in "${INSTANCES[@]}"
 do
     echo "instance is: $i"
-    if [ $i =="mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    if [ $i =="mongodb" ] | | [ $i == "mysql" ] | | [ $i == "shipping" ]
     then
         INSTANCES_type="t3.small"
     else
