c87e479d5962030200f4be83dea068b8e7507254
nandab1805 <balabhadranandakiranroyal@gmail.com>
2024-11-10 18:38:17 +0530

    Creating instances

--- Shell-Scripting/roboshopinstances.sh
+++ Shell-Scripting/roboshopinstances.sh
@@ -3,8 +3,8 @@
 AMI=ami-0b4f379183e5706b9 #this keeps on changing
 SG_ID=sg-03e57915a15c7d452 #replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-# ZONE_ID=Z104317737D96UJVA7NEF # replace your zone ID
-# DOMAIN_NAME="daws76s.online"
+ZONE_ID=Z0052926291UMF767L3HY # replace your zone ID
+DOMAIN_NAME="nanda.cfd"
 
 for i in "${INSTANCES[@]}"
 do
@@ -17,4 +17,22 @@ do
 
     IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-03e57915a15c7d452  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
+        #create R53 record, make sure you delete existing record
+    aws route53 change-resource-record-sets \
+    --hosted-zone-id $ZONE_ID \
+    --change-batch '
+    {
+        "Comment": "Creating a record set for cognito endpoint"
+        ,"Changes": [{
+        "Action"              : "UPSERT"
+        ,"ResourceRecordSet"  : {
+            "Name"              : "'$i'.'$DOMAIN_NAME'"
+            ,"Type"             : "A"
+            ,"TTL"              : 1
+            ,"ResourceRecords"  : [{
+                "Value"         : "'$IP_ADDRESS'"
+            }]
+        }
+        }]
+    }
 done
