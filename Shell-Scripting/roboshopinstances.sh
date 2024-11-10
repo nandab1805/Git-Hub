94c8d9cd6971c5939e1b8832081a14b65e80421d
nandab1805 <balabhadranandakiranroyal@gmail.com>
2024-11-10 18:19:46 +0530

    Creating instances

--- Shell-Scripting/roboshopinstances.sh
+++ Shell-Scripting/roboshopinstances.sh
@@ -6,16 +6,13 @@ INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipp
 
 for i in "${INSTANCES[@]}"
 do
-    
-    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    echo "instance is: $i"
+    if [ "$i" == "mongodb" ] || [ "$i" == "mysql" ] || [ "$i" == "shipping" ]
     then
         INSTANCES_type="t3.small"
     else
         INSTANCES_type="t2.micro"
     fi
-    
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
-    echo "$i: $IP_ADDRESS"
-    
-    # echo "$i: $IP_ADDRESS"
+    echo "Instance type: $INSTANCES_type"  # Debugging line to ensure it's set correctly
+    aws ec2 run-instances --image-id $AMI --instance-type $INSTANCES_type --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
 done
