d7912d52f8e7ad0a1c79899effe3751ca9c32ea1
nandab1805 <balabhadranandakiranroyal@gmail.com>
2024-11-10 18:04:37 +0530

    Creating instances

--- Shell-Scripting/roboshopinstances.sh
+++ Shell-Scripting/roboshopinstances.sh
@@ -13,6 +13,6 @@ do
     else
         INSTANCES_type="t2.micro"
     fi
-    aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCES_type  --security-group-ids sg-03e57915a15c7d452 --tag-specifications "ResourceType=instance,Tags=[{key=Name,Value=$i}]" 
+    aws ec2 run-instances --image-id ami-0b4f379183e5706b9 --instance-type $INSTANCES_type  --security-group-ids sg-03e57915a15c7d452 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" 
     # --query 'Instances[0].PrivateIpAddress' --output text
 done
