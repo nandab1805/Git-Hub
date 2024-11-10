9b199f533dd634ade73d109211578e0a949938fd
nandab1805 <balabhadranandakiranroyal@gmail.com>
2024-11-10 18:40:36 +0530

    Creating instances

--- Shell-Scripting/roboshopinstances.sh
+++ Shell-Scripting/roboshopinstances.sh
@@ -1,38 +1,38 @@
 #!/bin/bash
 
-AMI=ami-0b4f379183e5706b9 #this keeps on changing
-SG_ID=sg-03e57915a15c7d452 #replace with your SG ID
+AMI=ami-0b4f379183e5706b9  # this keeps on changing
+SG_ID=sg-03e57915a15c7d452  # replace with your SG ID
 INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
-ZONE_ID=Z0052926291UMF767L3HY # replace your zone ID
+ZONE_ID=Z0052926291UMF767L3HY  # replace your zone ID
 DOMAIN_NAME="nanda.cfd"
 
 for i in "${INSTANCES[@]}"
 do
-    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    if [[ $i == "mongodb" ]] || [[ $i == "mysql" ]] || [[ $i == "shipping" ]]
     then
         INSTANCE_TYPE="t3.small"
     else
         INSTANCE_TYPE="t2.micro"
     fi
 
-    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-03e57915a15c7d452  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
+    IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids $SG_ID  --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
-        #create R53 record, make sure you delete existing record
+
+    # Create R53 record, make sure you delete the existing record if needed
     aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
-    --change-batch '
-    {
-        "Comment": "Creating a record set for cognito endpoint"
-        ,"Changes": [{
-        "Action"              : "UPSERT"
-        ,"ResourceRecordSet"  : {
-            "Name"              : "'$i'.'$DOMAIN_NAME'"
-            ,"Type"             : "A"
-            ,"TTL"              : 1
-            ,"ResourceRecords"  : [{
-                "Value"         : "'$IP_ADDRESS'"
-            }]
-        }
+    --change-batch "{
+        \"Comment\": \"Creating a record set for $i endpoint\",
+        \"Changes\": [{
+            \"Action\": \"UPSERT\",
+            \"ResourceRecordSet\": {
+                \"Name\": \"$i.$DOMAIN_NAME\",
+                \"Type\": \"A\",
+                \"TTL\": 60,
+                \"ResourceRecords\": [{
+                    \"Value\": \"$IP_ADDRESS\"
+                }]
+            }
         }]
-    }
+    }"
 done
