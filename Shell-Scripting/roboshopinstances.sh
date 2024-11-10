b91c30f147eb7bfe2b78a23dcbd59bdb5b7b1354
nandab1805 <balabhadranandakiranroyal@gmail.com>
2024-11-10 18:23:42 +0530

    Creating instances

--- Shell-Scripting/roboshopinstances.sh
+++ Shell-Scripting/roboshopinstances.sh
@@ -17,3 +17,4 @@ do
 
     IP_ADDRESS=$(aws ec2 run-instances --image-id $AMI --instance-type $INSTANCE_TYPE --security-group-ids sg-087e7afb3a936fce7 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
     echo "$i: $IP_ADDRESS"
+done
